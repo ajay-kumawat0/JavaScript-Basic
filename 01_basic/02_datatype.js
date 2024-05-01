599eb0c8dc94fcaa69942cd8d64a9ce841f8cc65
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-01 10:40:38 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,23 @@
+// Data Types
+
+
+
+// Primitive Data Type
+let name = 'Ajay';
+let age = 20;
+let isLoggedIn = false;
+let number;
+let other = null;
+
+
+console.log(typeof name);  // string
+console.log(typeof age);    // number
+console.log(typeof isLoggedIn);   // boolean
+console.log(typeof number);  // undefine
+console.log(typeof other);  // object
+
+
+// Non-primitive Data Type 
+
+// Object
+// array
