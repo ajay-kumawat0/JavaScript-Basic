01ad8b2b70886245d7aae97f793c8d6aef544e6f
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-01 10:40:20 +0000

    varaiable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+const id = 8345546;
+let email = "aj@gmail.com";
+var name = "Ajay";
+otherName = 'Aj';
+
+// id = 3;  //You can't reassign const variable value;
+
+email = "ram@gmail.com";
+name = "Ram";
+otherName = "ramu"
+
+/*
+Do not use var for declaring any variable bcz it make issue in block scope and functional scope
+*/
+
+//console.log(id);
+
+console.table([id, email, name, otherName]);
