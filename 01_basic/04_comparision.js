532ddf736d27e8123ecdc5c8204dc7f022f51b3e
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-01 12:06:03 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>4);
+// console.log(2>=4);
+// console.log(2<4);
+// console.log(2<=4);
+// console.log(2==4);
+
+// console.log("5" > 2);
+
+
+// console.log(null > 0); //false
+// console.log(null >= 0);  // true  // Here type conversion is perform
+// console.log(null == 0); // false  // Here type conversion is not perform
+
+
+// console.log(undefined > 0); //false
+// console.log(undefined >= 0);  // false
+// console.log(undefined == 0); // false
