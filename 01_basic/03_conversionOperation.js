fc7812d521cb11f3c59143b48fa19e4cde519cec
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-01 11:11:50 +0000

    conversiionOperation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,75 @@
+// let age = "20";
+
+// console.log(typeof age); // string
+// console.log(typeof (age)); // string
+
+// let convertInt = Number(age); 
+// console.log(typeof convertInt);  // number
+// console.log(convertInt); // 20
+
+
+// let age = null;
+
+// console.log(typeof age); // object
+// console.log(typeof (age)); // object
+
+// let convertInt = Number(age);
+// console.log(typeof convertInt); // number
+// console.log(convertInt); // 0
+
+// let age = undefined;
+
+// console.log(typeof age); // undefined
+// console.log(typeof (age)); // undefined
+
+// let convertInt = Number(age);
+// console.log(typeof convertInt); // number
+// console.log(convertInt); // NaN
+
+
+// let age = '123abc';
+
+// console.log(typeof age); // string
+// console.log(typeof (age)); // string
+
+// let convertInt = Number(age);
+// console.log(typeof convertInt); // number
+// console.log(convertInt); // NaN
+
+
+/* ******************* 
+"20" => 20
+"123abc" => NaN
+true => 1 , false = > 0
+undefined => NaN
+null => 0
+
+********************/
+
+
+// let isAvailable = 1;
+// console.log(isAvailable); // 1
+
+// let booleanIsAvailable = Boolean(isAvailable);
+// console.log(booleanIsAvailable); // true
+
+
+// let isAvailable = 156;
+// console.log(isAvailable); // 156
+
+// let booleanIsAvailable = Boolean(isAvailable);
+// console.log(booleanIsAvailable); // true
+
+
+// let isAvailable = "";
+// console.log(isAvailable); // 
+
+// let booleanIsAvailable = Boolean(isAvailable);
+// console.log(booleanIsAvailable); // false
+
+
+// let isAvailable = "ram";
+// console.log(isAvailable); // ram
+
+// let booleanIsAvailable = Boolean(isAvailable);
+// console.log(booleanIsAvailable); // true
