2d6e249aa93844c5bd788758ee679a91662e9df0
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-02 06:40:33 +0000

    date in js

---
+++ 01_basic/07_dateInJs.js
@@ -0,0 +1,10 @@
+// const dates = new Date();
+// console.log(dates); //2024-05-02T06:21:50.887Z
+// console.log(dates.toString()); //Thu May 02 2024 06:22:28 GMT+0000 (Coordinated Universal Time)
+// console.log(dates.toISOString()); //2024-05-02T06:22:56.970Z
+// console.log(dates.toJSON()); //2024-05-02T06:22:56.970Z
+// console.log(dates.toLocaleDateString()); //5/2/2024
+// console.log(dates.toLocaleString()); //5/2/2024, 6:24:29 AM
+
+// const myDate = new Date(2024,4,2);
+// console.log(myDate.toDateString());
