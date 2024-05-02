f90702bfb10a4d148d93e6a199d45db34cc5a634
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-02 09:24:56 +0000

    objects

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -8,11 +8,11 @@ obj.email = "raj@google.com"
 // console.log(obj);
 
 const obj1 = {
-    name : 'ram',
-    obj2 : {
-        age : 20,
-        obj3 : {
-            email : "raj@gmail.com"
+    name: 'ram',
+    obj2: {
+        age: 20,
+        obj3: {
+            email: "raj@gmail.com"
         }
     }
 }
@@ -20,16 +20,26 @@ const obj1 = {
 // console.log(obj1);
 
 
-const ob1 = {1:'a', 2:'b'}
-const ob2 = {3:'a', 4:'b'}
-const ob3 = {ob1, ob2}
+const ob1 = { 1: 'a', 2: 'b' }
+const ob2 = { 3: 'a', 4: 'b' }
+const ob3 = { ob1, ob2 }
 // console.log(ob3);
 
-const ob4 = Object.assign(ob1,ob2); // this is used to concate the two object
-const ob5 = {...ob1, ...ob2}
+const ob4 = Object.assign(ob1, ob2); // this is used to concate the two object
+const ob5 = { ...ob1, ...ob2 }
 // console.log(ob5);
 
 // console.log(obj);
 // console.log(Object.keys(obj));
 // console.log(Object.values(obj));
 // console.log(Object.entries(obj));
+
+const users = {
+    name : "Raj",
+    age : 20,
+    email : "raj@google.com"
+}
+
+// console.log(users.name);
+const {name} = users; // destructing
+const {name : username} = users; // destructing and also change name for calling
