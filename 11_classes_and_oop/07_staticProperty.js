4a86867737c14d7eaa19791f816ca4f2b85e02fa
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-08 04:57:09 +0000

    Static Property

---
+++ 11_classes_and_oop/07_staticProperty.js
@@ -0,0 +1,31 @@
+class Admin {
+    constructor(adminName) {
+        this.adminName = adminName;
+    }
+
+    show() {
+        return `Admin name is ${this.adminName}`;
+    }
+
+    static adminId() {  // static is help to not access in extends class
+        return `Admin id is 2121`;
+    }
+}
+
+class raj extends Admin{
+    constructor(adminName){
+        super(adminName)
+    }
+
+    getName(){
+        return this.adminName;
+    }
+}
+
+// const ad = new Admin("Ram");
+// console.log(ad.show());
+
+
+const raj1 = new raj("Ram");
+console.log(raj1.show());
+console.log(raj1.adminId());
