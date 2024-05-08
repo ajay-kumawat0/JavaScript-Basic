4a32dd643116279befe3ae5a7c2fbfb9f564eff6
Ajay Kumawat <168632304+ajay-kumawat0@users.noreply.github.com>
2024-05-08 08:08:34 +0000

    Previse Setter and Getter

--- 11_classes_and_oop/11_previse_get_set.js
+++ 11_classes_and_oop/11_previse_get_set.js
@@ -1,1 +1,24 @@
-// function 
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+}
+
+const user1 = new User("ram@fb.com", "ram")
+console.log(user1.email);
